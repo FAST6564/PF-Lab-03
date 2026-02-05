88a380319dd18b7186a8a02059117d85254b38b0
FAST6564 <k256564@nu.edu.pk>
2026-02-05 17:02:20 +0500

    Create variables.c

---
+++ variables.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int age = 25;
+    float price = 77.99;
+    double distance = 1874.5078;
+    char grade = 'A';
+    printf("Int: %d, Float: %.2f, Double: %.4lf, Char: %c\n", age, price, distance, grade);
+    return 0;
+}
